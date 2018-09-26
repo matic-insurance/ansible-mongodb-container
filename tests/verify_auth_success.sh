33679402e44921c161eb9c204bcdc04bf8debb10
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2018-09-26 20:09:47 +0300

    Support user authentication

--- tests/verify_auth_success.sh
+++ tests/verify_auth_success.sh
@@ -7,8 +7,8 @@ COMMAND="db.list.insert({test: 1}); print('records: '+db.list.count())"
 #No authentication db
 mongo ${DB} --host localhost --port 2701 --eval "${COMMAND}"
 #Admin only db
-mongo ${DB} --host localhost --port 2702 --username "admin" --password "admin_pass" --authenticationDatabase "admin" --eval "${COMMAND}"
+mongo ${DB} --host localhost --port 2702 -u "admin" -p "admin_pass" --authenticationDatabase "admin" --eval "${COMMAND}"
 #Admin && User db
-mongo ${DB} --host localhost --port 2703 --username "admin" --password "admin_pass" --authenticationDatabase "admin" --eval "${COMMAND}"
-mongo ${DB} --host localhost --port 2703 --username "app" --password "app_pass" --eval "${COMMAND}"
+mongo ${DB} --host localhost --port 2703 -u "admin" -p "admin_pass" --authenticationDatabase "admin" --eval "${COMMAND}"
+mongo ${DB} --host localhost --port 2703 -u "app" -p "app_pass" --eval "${COMMAND}"
 
